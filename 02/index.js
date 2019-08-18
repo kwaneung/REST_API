6c78f85066ea583a732d7d3e632b2be8a49d5c06
kwaneung <42456925+kwaneung@users.noreply.github.com>
2019-08-19 00:18:07 +0900

    TDD로 API 서버 개발
    실행은 /bin/www.js로 분리
    limit으로 받을 갯수 정하는것과 정수형이 아닐경우 예회처리

--- 02/index.js
+++ 02/index.js
@@ -2,19 +2,31 @@ const express = require('express')
 const logger = require('morgan')
 const app = express()  // 어플리케이션
 const users = [  // 객체 생성
-    {name: 'kwaneung'},
-    {name: 'hyunyoung'},
+    {id: 1, name: 'Kwaneung'},
+    {id: 2, name: 'hyunyoung'},
+    {id: 3, name: 'Chirs'}
 ]
+
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/users', (req, res) => res.json(users))
-app.post('/users', (req, res) => res.json(users))
-app.get('/users', (req, res) => res.json(users))
-app.delete('/users', (req, res) => res.json(users))
-
-// app.listen(3000, () => console.log('running'))
+app.get('/users', (req, res) => {
+    // limit 갯수만큼만 user들을 출력할 수 있도록해보자
+    req.query.limit = req.query.limit || 10  // limit이 없으면 default로 10을 넣어줌
+    const limit = parseInt(req.query.limit, 10)  // 10진수 문자열로 변환
+    // limit이 숫자형이 아니면 400을 응답
+    // offset이 숫자형이 아니면 400을 응답
+    // console.log(limit)
+    if(Number.isNaN(limit)){
+        res.status(400).end()
+    }else{
+        res.json(users.slice(0, limit))  // 0번부터 limit 갯수만큼 출력
+    }
+})
+// app.post('/users', (req, res) => res.json(users))
+// app.get('/users', (req, res) => res.json(users))
+// app.delete('/users', (req, res) => res.json(users))
 
 module.exports = app  // app을 외부에서도 볼 수 있도록
--- 02/index.spec.js
+++ 02/index.spec.js
@@ -8,21 +8,41 @@ const app = require('./index')  // index.js에 있는 app을 모듈형식으로 
 describe('GET /users', () => {
     // 여기에 테스트코드 작성
     // it도 mocha가 가지고있는 함수
-    it('test code\n', (done) => {
-        // assert.equal(1, 0)  // 파라미터1과 파라미터2가 같음
+    describe('성공', () => {
 
-        // (1).should.equal(1)  // done() 써줘야함
-        // done()
-
-        request(app)  // supertest가 알아서 listen까지함
-            .get('/users')
-            .end((err, res) => {
-                console.log(res.body)
-                res.body.should.be.instanceof(Array)  // 배열 체크
-                res.body.forEach(user => {
-                    user.should.have.property('name')
+        it('test code\n', (done) => {
+            // assert.equal(1, 0)  // 파라미터1과 파라미터2가 같음
+    
+            // (1).should.equal(1)  // done() 써줘야함
+            // done()
+    
+            request(app)  // supertest가 알아서 listen까지함
+                .get('/users')
+                .end((err, res) => {
+                    console.log(res.body)
+                    res.body.should.be.instanceof(Array)  // 배열 체크
+                    res.body.forEach(user => {
+                        user.should.have.property('name')
+                    });
+                    done()
                 });
-                done()
-            });
+        })
+        it('최대 limit 갯수만큼 응답한다', done => {
+                request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    console.log(res.body)
+                    res.body.should.have.lengthOf(2)
+                    done() 
+                })
+        })
+    })
+    describe('실패', () => {
+        it('limit이 정수가 아니면 400을 응답한다', done => {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done)
+        })
     })
 })
