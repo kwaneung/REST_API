62e476f0bfe5827991daff54aa4b41ada54e1324
kwaneung <42456925+kwaneung@users.noreply.github.com>
2019-08-19 01:54:36 +0900

    사용자목록 API 개발
    GET, DELETE, POST에대한 각각의 라우터들을 추가해서 supertest로 성공, 실패 경우 테스트

--- 02/index.js
+++ 02/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const logger = require('morgan')
+const bodyParser = require('body-parser')
 const app = express()  // 어플리케이션
-const users = [  // 객체 생성
+let users = [  // 객체 생성
     {id: 1, name: 'Kwaneung'},
     {id: 2, name: 'hyunyoung'},
     {id: 3, name: 'Chirs'}
@@ -9,24 +10,73 @@ const users = [  // 객체 생성
 
 
 app.use(logger('dev'))
+app.use(bodyParser.json())  // json 타입으로 바디를 받는다
+app.use(bodyParser.urlencoded({ extended: true }))  // 인코딩 옵션
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/users', (req, res) => {
     // limit 갯수만큼만 user들을 출력할 수 있도록해보자
+    // ?뒤로 오면 쿼리로 받음
     req.query.limit = req.query.limit || 10  // limit이 없으면 default로 10을 넣어줌
     const limit = parseInt(req.query.limit, 10)  // 10진수 문자열로 변환
     // limit이 숫자형이 아니면 400을 응답
     // offset이 숫자형이 아니면 400을 응답
     // console.log(limit)
-    if(Number.isNaN(limit)){
+    if (Number.isNaN(limit)){
         res.status(400).end()
     }else{
         res.json(users.slice(0, limit))  // 0번부터 limit 갯수만큼 출력
     }
 })
-// app.post('/users', (req, res) => res.json(users))
+app.get('/users/:id', (req, res) => {  // :id 는 id변수인식
+    // id 값을 얻어낸다
+    // url path안에 데이터를 넘겨줄때 params이용
+    const id = parseInt(req.params.id, 10)
+    if (Number.isNaN(id)) {
+        return res.status(400).end()  // return을 해줘야 아래 로직이 실행 안된다.
+    }
+
+    // users 배열 조회
+    const user = users.filter(user => user.id === id)[0]
+    if (!user) {
+        return  res.status(404).end()
+    }
+
+    // 응답: res객체 이용
+    res.json(user)
+
+
+    //id가 숫자가 아닐 경우 400응답
+    //id가 없는 유저의 경우 404응답
+})
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    if (Number.isNaN(id)) {
+        return res.status(400).end()
+    }
+    users = users.filter(user => user.id !== id)
+    res.status(204).end()
+})
+app.post('/users', (req, res) => {
+    // client에서 보낸 데이터 id는 서버관리
+    const name = req.body.name
+    if (!name) {
+        return res.status(400).end()
+    }
+    
+    const found = users.filter(user => user.name === name).length
+    if (found) {
+        return res.status(409).end()
+    }
+
+    const id = Date.now()  // 원래는 DB 에서 AutoInc
+
+    const user = {id, name}
+    users.push(user)
+    res.status(201).json(user)
+
+})
 // app.get('/users', (req, res) => res.json(users))
-// app.delete('/users', (req, res) => res.json(users))
 
 module.exports = app  // app을 외부에서도 볼 수 있도록
--- 02/index.spec.js
+++ 02/index.spec.js
@@ -19,7 +19,7 @@ describe('GET /users', () => {
             request(app)  // supertest가 알아서 listen까지함
                 .get('/users')
                 .end((err, res) => {
-                    console.log(res.body)
+                    // console.log(res.body)
                     res.body.should.be.instanceof(Array)  // 배열 체크
                     res.body.forEach(user => {
                         user.should.have.property('name')
@@ -31,7 +31,7 @@ describe('GET /users', () => {
                 request(app)
                 .get('/users?limit=2')
                 .end((err, res) => {
-                    console.log(res.body)
+                    // console.log(res.body)
                     res.body.should.have.lengthOf(2)
                     done() 
                 })
@@ -42,6 +42,83 @@ describe('GET /users', () => {
             request(app)
                 .get('/users?limit=two')
                 .expect(400)
+                .end(done)
+        })
+    })
+})
+describe('GET /users/:id', () => {
+    describe('성공', () => {
+        it('유저 객체를 반환한다', done => {
+            request(app)
+                .get('/users/2')
+                .end((err, res) => {
+                    // console.log(res.body)
+                    res.body.should.have.property('id', 2)
+                    done()
+                })
+        })
+    })
+    describe('실패', () => {
+        //id가 숫자가 아닐 경우 400응답
+        //id가 없는 유저의 경우 404응답
+        it('id가 숫자가 아닐 경우 400 응답', done => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done)
+        })
+        it('찾을 수 없는 id인 경우 404 응답', done => {
+            request(app)
+                .get('/users/9')
+                .expect(404)
+                .end(done)
+        })
+    })
+})
+describe('DELETE /users/:id', () => {
+    describe('성공', () => {
+        it('204 응답', done => {
+            request(app)
+                .delete('/users/3')
+                .expect(204)
+                .end(done)
+        })
+    })
+    describe('실패', () => {
+        it('id가 숫자가 아닌경우 400', done => {
+            request(app)
+                .delete('/users/three')
+                .expect(400)
+                .end(done)
+        })
+    })
+})
+describe('POST /users', () => {
+    describe('성공', () => {
+        it('생성된 유저 객체와 201을 응답', done => {
+            request(app)
+            .post('/users')
+            .send({name: 'Daniel'})
+            .expect(201)
+            .end((err, res) => {
+                res.body.should.have.property('name', 'Daniel')
+                done()
+            })
+        })
+    })
+    describe('실패', () => {
+        it('name 누락시 400 응답', done => {
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        })
+        it('name 중복시 409 응답', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'Kwaneung'})
+                .expect(409)
                 .end(done)
         })
     })
